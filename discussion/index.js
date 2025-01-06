df04aa26b0493226c93b0fcdd319de0be7f45a5c
Carl Gabriel S. Madriaga <csmadriaga@SSCGI.COM>
2025-01-06 10:42:58 +0800

    add complex function

--- discussion/index.js
+++ discussion/index.js
@@ -8,7 +8,7 @@ printName();
 
 //FUNCTION EXPRESSION
 let variableFunction = function () {
-  Console.log("sadsa");
+  console.log("ppp");
 };
 
 variableFunction();
@@ -16,3 +16,37 @@ variableFunction();
 const constFunc = function () {
   console.log("initialized with const");
 };
+
+//Parameter and Arguments
+
+function printName(name) {
+  console.log("My name is " + name);
+}
+
+printName("Juan"); // this is called Arguments
+
+// "name" is called parameter
+// A Parameter acts as a named variable
+// containers that exits only inside of a function
+// it is used to store information that is provided to a function
+// wheb it is called
+
+// An argument is a value passed when invoking a function,
+// this argument is then stored as the paramenter within the function
+
+printName("CHICHI");
+printName("POOF", 12);
+
+function argumentFunction() {
+  console.log(
+    "this function was passed as argument before the message was printed."
+  );
+}
+
+function invokeFunction(argumentFunction) {
+  argumentFunction();
+}
+
+invokeFunction(argumentFunction);
+// invokeFunction();
+// console.log(argumentFunction);
