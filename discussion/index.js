b047ae44e939abee1f0d24a1d4c3097f13d44bc7
Carl Gabriel S. Madriaga <csmadriaga@SSCGI.COM>
2025-01-06 10:06:56 +0800

    update the function lectures

---
+++ discussion/index.js
@@ -0,0 +1,18 @@
+// console.log("hi");
+
+function printName() {
+  console.log("My name is Carl");
+}
+
+printName();
+
+//FUNCTION EXPRESSION
+let variableFunction = function () {
+  Console.log("sadsa");
+};
+
+variableFunction();
+
+const constFunc = function () {
+  console.log("initialized with const");
+};
